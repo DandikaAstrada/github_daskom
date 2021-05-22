ce9500e984f63c4a7357dd252f15d8ad7d625835
DandikaAstrada <dandikapermana@gmail.com>
2021-05-22 13:12:03 +0700

    project saya

---
+++ resources/views/page2.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page2</title>
+</head>
+<body>
+    <p>Sesuai dengan namanya sendiri, framework adalah kerangka kerja untuk mengembangkan aplikasi berbasis website maupun desktop. Kerangka kerja disini sangat membantu developer dalam menuliskan sebuah dengan lebih terstruktur dan tersusun rapi.
+
+Kerangka kerja diciptakan untuk mempermudah kinerja dari programmer. Sehingga, seorang programmer tidak perlu untuk menuliskan kode secara berulang - ulang. Karena di dalamnya sendiri anda hanya perlu menyusun komponen - komponen pemrograman saja.</p>
+</body>
+</html>
---
+++ resources/views/page3.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page3</title>
+</head>
+<body>
+
+    <p>
+    Nama    : Dandika Astrada Yamandaka Permana
+    <br>
+    Kontak  : 081455029791
+    <br>
+    Email   : dandikapermana@gmail.com
+    <br>
+    Sosmed  : IG = @dnastryman</p>
+    <img src="{{ ('Fotoku.JPEG') }}" >
+</body>
+</html>
